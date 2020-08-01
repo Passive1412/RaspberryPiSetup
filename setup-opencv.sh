531af67811871e6ec25767daf291b3a4cef22478
Marcus <46575460+Passive1412@users.noreply.github.com>
2020-08-01 20:06:20 +0200

    Update setup-opencv.sh

--- setup-opencv.sh
+++ setup-opencv.sh
@@ -12,3 +12,49 @@ if [ "$(whoami)" != "root" ]; then
     echo "Sorry, this script must be executed with sudo or as root"
     exit 1
 fi
+
+echo
+echo "----------------"
+echo "Updating and upgrading sources"
+echo "----------------"
+echo
+
+apt-get update -qq
+apt-get upgrade -qq
+
+echo
+echo "----------------"
+echo "Purging unneeded libraries"
+echo "----------------"
+echo
+apt-get -y purge BlueJ
+apt-get -y purge Claws Mail
+apt-get -y purge Greenfoot
+apt-get -y purge libreoffice*
+apt-get -y purge Mathematica
+apt-get -y purge Minecraft
+apt-get -y purge Node-RED
+apt-get -y purge Node-Scratch
+apt-get -y purge Node-Strach2
+apt-get -y purge Sense SmartSim
+apt-get -y purge Sense Sonic Pi
+apt-get -y purge wolfram-engine
+apt-get -y clean
+apt-get -y autoremov
+
+
+echo
+echo "----------------"
+echo "Update & Upgrade"
+echo "----------------"
+echo
+apt-get -y update
+apt-get -y upgrade
+
+echo
+echo "-----------------------"
+echo "Installing dependencies"
+echo "-----------------------"
+echo
+
+
