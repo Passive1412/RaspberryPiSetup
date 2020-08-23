b4c77f77f57a477f79331362c7c704da39d2834d
Passive1412 <marcusjhomann@outlook.com>
2020-08-23 12:54:37 +0200

    added comment

--- setup-opencv.sh
+++ setup-opencv.sh
@@ -93,40 +93,40 @@
 # mv opencv-4.4.0 opencv
 # mv opencv_contrib-4.4.0 opencv_contrib
 
-echo
-echo "-----------------------"
-echo "Begin Compile OpenCV"
-echo "-----------------------"
-echo
+# echo
+# echo "-----------------------"
+# echo "Begin Compile OpenCV"
+# echo "-----------------------"
+# echo
 
-cd ~/opencv/
-mkdir build
-cd build
+# cd ~/opencv/
+# mkdir build
+# cd build
 
-cmake -D CMAKE_BUILD_TYPE=RELEASE \
-        -D CMAKE_INSTALL_PREFIX=/usr/local \
-        -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
-        -D ENABLE_NEON=ON \
-        -D ENABLE_VFPV3=ON \
-        -D WITH_OPENMP=ON \
-        -D BUILD_TIFF=ON \
-        -D WITH_FFMPEG=ON \
-        -D WITH_GSTREAMER=ON \
-        -D WITH_TBB=ON \
-        -D BUILD_TBB=ON \
-        -D BUILD_TESTS=OFF \
-        -D WITH_EIGEN=OFF \
-        -D WITH_V4L=ON \
-        -D WITH_LIBV4L=ON \
-        -D WITH_VTK=OFF \
-        -D OPENCV_EXTRA_EXE_LINKER_FLAGS=-latomic \
-        -D OPENCV_ENABLE_NONFREE=ON \
-        -D INSTALL_C_EXAMPLES=OFF \
-        -D INSTALL_PYTHON_EXAMPLES=OFF \
-        -D BUILD_NEW_PYTHON_SUPPORT=ON \
-        -D BUILD_opencv_python3=TRUE \
-        -D OPENCV_GENERATE_PKGCONFIG=ON \
-        -D BUILD_EXAMPLES=OFF ..
+# cmake -D CMAKE_BUILD_TYPE=RELEASE \
+#         -D CMAKE_INSTALL_PREFIX=/usr/local \
+#         -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+#         -D ENABLE_NEON=ON \
+#         -D ENABLE_VFPV3=ON \
+#         -D WITH_OPENMP=ON \
+#         -D BUILD_TIFF=ON \
+#         -D WITH_FFMPEG=ON \
+#         -D WITH_GSTREAMER=ON \
+#         -D WITH_TBB=ON \
+#         -D BUILD_TBB=ON \
+#         -D BUILD_TESTS=OFF \
+#         -D WITH_EIGEN=OFF \
+#         -D WITH_V4L=ON \
+#         -D WITH_LIBV4L=ON \
+#         -D WITH_VTK=OFF \
+#         -D OPENCV_EXTRA_EXE_LINKER_FLAGS=-latomic \
+#         -D OPENCV_ENABLE_NONFREE=ON \
+#         -D INSTALL_C_EXAMPLES=OFF \
+#         -D INSTALL_PYTHON_EXAMPLES=OFF \
+#         -D BUILD_NEW_PYTHON_SUPPORT=ON \
+#         -D BUILD_opencv_python3=TRUE \
+#         -D OPENCV_GENERATE_PKGCONFIG=ON \
+#         -D BUILD_EXAMPLES=OFF ..
 
 echo
 echo "-----------------------"
